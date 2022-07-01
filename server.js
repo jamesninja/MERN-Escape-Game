5432771c378052935f5a65c8cd3c772320568047
osekisi <osekisinde@gmail.com>
2022-07-01 10:57:33 +0200

    Suppression App et mise à jour server.js

--- server.js
+++ server.js
@@ -25,21 +25,3 @@ app.listen(PORT, () => {
 })
 
 
-
-
-
-// const express = require('express')
-// const app = express()
-// const port = 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   mongoose.connect('mongodb+srv://TokenAlpha:GtPkohHwZt9drVi9@cluster0.zcmvm.mongodb.net/MERNEscapeGame?retryWrites=true&w=majority')
-//   console.log(`Example app listening on port ${port}`)
-// })
-
-
-
